485c6e3315b2ef24e63bd45744e6d458e74be0c0
Pete Seeling <peteseeling@gmail.com>
2022-04-28 17:21:37 -0600

    list of characters/quotes properly rendering.

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -10,24 +10,29 @@ export default function List(){
             const res = await fetch('https://futuramaapi.herokuapp.com/api/quotes');
             const result = await res.json();
 
-            console.log(result, 'result')
-
             const quotes = result.map((quote) => ({
                 quote: quote.quote,
+                character: quote.character
             }));
+        
             setQuotes(quotes)
+           
         }
         getAllQuotes();
 
         },[])
         if(loading){
             return(
-                
+                quotes.map((quote, i) => {
+
+               return(
                 <div>
-                <h2>{quotes.quote}</h2>
+                <h2>Name: {quote.character}</h2>
+                <h3>Quote: {quote.quote}</h3>
                 </div>
-            )}
+            )}))
+            
 }
         
     
-    
+}

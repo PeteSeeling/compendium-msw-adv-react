da4a0bfc4fe5f9bee3c16e64f25fdaa031eb44c3
Pete Seeling <peteseeling@gmail.com>
2022-04-28 13:51:37 -0600

    quotes fetching from api on list.jsx

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -3,5 +3,31 @@ import { useEffect, useState } from 'react';
 
 export default function List(){
     const [quotes, setQuotes] = useState([]);
-    
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        async function getAllQuotes(){
+            const res = await fetch('https://futuramaapi.herokuapp.com/api/quotes');
+            const result = await res.json();
+
+            console.log(result, 'result')
+
+            const quotes = result.map((quote) => ({
+                quote: quote.quote,
+            }));
+            setQuotes(quotes)
+        }
+        getAllQuotes();
+
+        },[])
+        if(loading){
+            return(
+                
+                <div>
+                <h2>{quotes.quote}</h2>
+                </div>
+            )}
 }
+        
+    
+    
